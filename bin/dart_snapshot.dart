42155c9a17be3a8008cf4fde0cc251cecbfeb60d
kuntydev <54059328+Ryanprw@users.noreply.github.com>
2025-03-07 23:49:13 +0700

    initial commit

---
+++ bin/dart_snapshot.dart
@@ -0,0 +1,5 @@
+import 'package:dart_snapshot/dart_snapshot.dart' as dart_snapshot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_snapshot.calculate()}!');
+}
---
+++ test/dart_snapshot_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_snapshot/dart_snapshot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
